a206cf82899cda3f0ee4691e41fd603b3027941a
Qheuss <36819862+Qheuss@users.noreply.github.com>
2024-09-16 09:24:52 +0200

    dotnet backend done

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30b5d6d43f01280e327e19d75ec0e66f71e0e19b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89106962b829dbe0688bdcb5f31f8459dc0eca9d")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
